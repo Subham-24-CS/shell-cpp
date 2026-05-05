321871643ed041859878dba052c286b110403217
Subham Das <subham@SubhamDas.localdomain>
2026-05-05 13:24:19 +0000

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main() {
   // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  cout << unitbuf;
+  cerr << unitbuf;
 
   
-  std::cout << "$ ";
+  cout << "$ ";
+  string command;
+  getline(std::cin, command);
+  cout << command  << ": command not found" << endl;
 }

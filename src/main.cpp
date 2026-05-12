201555bcccd4279b4ab1cac48139bd7ffc9c39cf
Subham Das <subham@SubhamDas.localdomain>
2026-05-12 11:27:48 +0000

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <vector>
 #include <unistd.h>
+#include <sys/wait.h>
 
 using namespace std;
 
 int main() {
-  // Flush after every std::cout / std:cerr
+
   cout << unitbuf;
   cerr << unitbuf;
 
@@ -14,6 +16,7 @@ int main() {
     cout << "$ ";
     string command;
     getline(std::cin, command);
+
     if(command == "exit") {
       break;
     }
@@ -26,7 +29,6 @@ int main() {
         cout << com << " is a shell builtin" << endl;
       } 
       else {
-        // Path search logic starts here
         bool found = false;
         char* path_env = std::getenv("PATH");
         if (path_env) {
@@ -47,7 +49,56 @@ int main() {
       }
     }
     else {
-      cout << command  << ": command not found" << endl;
+
+      stringstream ss(command);
+      string exe_name;
+      ss >> exe_name;
+
+      bool found = false;
+      string executable_path;
+      char* path_env = std::getenv("PATH");
+
+      if (path_env) {
+        stringstream ss_path(path_env);
+        string path_dir;
+        while (getline(ss_path, path_dir, ':')) {
+          string full_path = path_dir + '/' + exe_name;
+          if (access(full_path.c_str(), X_OK) == 0) {
+            executable_path = full_path;
+            found = true;
+            break;
+          }
+        }
+      }
+
+      if (found) {
+        // Collect arguments for execvp
+        vector<string> args_vec;
+        args_vec.push_back(exe_name);
+        string arg;
+        while (ss >> arg) {
+          args_vec.push_back(arg);
+        }
+
+        vector<char*> c_args;
+        for (auto& s : args_vec) {
+          c_args.push_back(&s[0]);
+        }
+        c_args.push_back(nullptr);
+
+        pid_t pid = fork();
+        if (pid == 0) {
+          // Child process
+          execvp(c_args[0], c_args.data());
+          exit(1); 
+        } else {
+          // Parent process
+          waitpid(pid, nullptr, 0);
+        }
+      } else {
+        cout << command << ": command not found" << endl;
+      }
     }
   }
+  return 0;
 }

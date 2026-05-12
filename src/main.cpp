5cfcc06c7b61905ec86f1d2fda2802f7385dbfc6
Subham Das <subham@SubhamDas.localdomain>
2026-05-12 11:20:16 +0000

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <unistd.h>
+
 using namespace std;
 
 int main() {
@@ -19,8 +22,29 @@ int main() {
     }
     else if(command.substr(0,5) == "type "){
       string com = command.substr(5);
-      if(com=="exit" || com=="type" || com=="echo") cout << com << " is a shell builtin" << endl;
-      else cout << com << ": not found" << endl;
+      if(com=="exit" || com=="type" || com=="echo") {
+        cout << com << " is a shell builtin" << endl;
+      } 
+      else {
+        // Path search logic starts here
+        bool found = false;
+        char* path_env = std::getenv("PATH");
+        if (path_env) {
+          std::stringstream ss_path(path_env);
+          std::string path;
+          while (std::getline(ss_path, path, ':')) {
+            std::string full_path = path + '/' + com;
+            if (access(full_path.c_str(), X_OK) == 0) {
+              std::cout << com << " is " << full_path << std::endl;
+              found = true;
+              break;
+            }
+          }
+        }
+        if (!found) {
+          cout << com << ": not found" << endl;
+        }
+      }
     }
     else {
       cout << command  << ": command not found" << endl;
